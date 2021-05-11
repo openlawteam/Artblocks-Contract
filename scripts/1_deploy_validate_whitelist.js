23d77d3c116dbdf1028857779eecb1a8615263dc
Craig Blake <craigwblake@gmail.com>
2021-05-11 15:09:42 -0400

    Adding deploy scripts.

--- scripts/1_deploy_validate_whitelist.js
+++ scripts/1_deploy_validate_whitelist.js
@@ -1,19 +1,18 @@
 const {BigNumber} = require('ethers');
-var prompt = require('prompt-sync')();
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   const deployerAddress = await deployer.getAddress();
   console.log(
-    "Deploying GEN ART 721 Minter from:",
+    "Deploying ValidateWhitelist from:",
     deployerAddress
   );
 
   const genArt721Address = prompt('GenArt721 address? ');
-  const GenArt721 = await ethers.getContractFactory("GenArt721Minter3");
-  const genArt721 = await GenArt721.deploy(genArt721Address);
+  const ValidateWhitelist = await ethers.getContractFactory("ValidateWhitelist");
+  const validateWhitelist = await ValidateWhitelist.deploy(genArt721Address);
 
-  console.log('GenArt721 Minter contract deployed at:', (await genArt721.deployed()).address);
+  console.log('ValidateWhitelist contract deployed at:', (await validateWhitelist.deployed()).address);
 }
 
 main()
